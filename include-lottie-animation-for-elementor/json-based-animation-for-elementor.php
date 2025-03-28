2914f5b5e2267df641253d000640a82921d787cc
Sean Murphy <sean@iamseanmurphy.com>
2025-03-28 03:25:19 +0000

    1.10.16

--- include-lottie-animation-for-elementor/json-based-animation-for-elementor.php
+++ include-lottie-animation-for-elementor/json-based-animation-for-elementor.php
@@ -3,12 +3,12 @@
 * Plugin Name: JSON Based Animation for Elementor
 * Plugin URI: http://lottieanimation.kapasias.com/
 * Description: Autoplay | Hover | Click | Mouse Over-Out | Parallax Effect using JSON Based Animation for Elementor and proudly developed by KAP ASIAs Team.
-* Version: 1.10.15
+* Version: 1.10.16
 * Author: KAP ASIAs
 * Author URI: http://kapasias.com
 * Text Domain: jbafe
-* Elementor tested up to: 3.26
-* Elementor Pro tested up to: 3.26
+* Elementor tested up to: 3.28
+* Elementor Pro tested up to: 3.28
 */
 
 // Prevent direct access to files
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 // Plugin version
-defined( 'JBAFE_VERSION' ) or define( 'JBAFE_VERSION', '1.10.15' );
+defined( 'JBAFE_VERSION' ) or define( 'JBAFE_VERSION', '1.10.16' );
 define('JBAFE_PATH', plugin_dir_path(__FILE__));
 define('JBAFE_URL', plugin_dir_url(__FILE__));
 
--- include-lottie-animation-for-elementor/readme.txt
+++ include-lottie-animation-for-elementor/readme.txt
@@ -4,7 +4,7 @@ Tags: elementor, elementor addon, lottie, bodymovin, animation
 Requires at least: 5.7.0
 Tested up to: 6.7
 Requires PHP: 5.6
-Stable tag: 1.10.15
+Stable tag: 1.10.16
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -74,6 +74,9 @@ Absolutely. It will work with any theme where Elementor works.
 
 
 == Changelog ==
+
+= 1.10.16 =
+* Compatibility : Elementor 3.28
 
 = 1.10.15 =
 * Compatibility : Wordpress 6.7
